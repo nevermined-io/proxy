a4ded568531b18b3de2becfcfb28cc438e3c40a8
enrique <enrique@nevermined.io>
2023-11-23 11:30:38 +0100

    fix: rsyslog initialization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 nginx &
 
+# Preparing Rsyslog
+conf/rsyslog/51-upstream.template.conf /etc/rsyslog.d/51-upstream.conf
+
+sed -i "s|PG_HOST|$PG_HOST|g" /etc/rsyslog.d/51-upstream.conf
+sed -i "s|PG_USER|$PG_USER|g" /etc/rsyslog.d/51-upstream.conf
+sed -i "s|PG_PASSWORD|$PG_PASSWORD|g" /etc/rsyslog.d/51-upstream.conf
+sed -i "s|PG_DB|$PG_DB|g" /etc/rsyslog.d/51-upstream.conf
+
 service rsyslog restart
 
 yarn start:oauth-server 
